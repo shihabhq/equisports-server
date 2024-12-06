ac81d081dd50462fe944318937cba495918a362c
Shihab Haque <adnansaim15@yahoo.com>
2024-12-06 12:10:58 +0600

    initial server created

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const CreateEquipment = async (req, res) => {};
+
+const getHomeProducts = async (req, res) => {};
+const getMyProducts = async (req, res) => {};
+
+const viewSingleProduct = async (req, res) => {};
+
+const updateProducts = async (req, res) => {};
+
+const deleteProducts = async (req, res) => {};
